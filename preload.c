caae803a202daab344d34631460b660a58fb1653
TheAssassin <theassassin@assassinate-you.net>
2020-09-27 21:54:22 +0200

    Add global init function to preloaded lib
    Reduces amount of duplicate code and slightly improves performance (probably negligible, but still).

--- preload.c
+++ preload.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <errno.h>
+#include <stdbool.h>
 
 // own headers
 #include "logging.h"
@@ -21,24 +22,44 @@
 // TODO: move into central header, it's the same value in main.cpp
 #define EXIT_CODE_FAILURE 0xff
 
+// pointers to actual implementations in libc
+// will be initialized by __init()
+// TODO: create a macro for this pattern (DRY)
+static char* (*__libc_realpath)(const char*, char*) = NULL;
+static int (*__libc_open)(const char*, int) = NULL;
+static ssize_t (*__libc_readlink)(const char*, void*, size_t) = NULL;
+
 // TODO: write __init() and call that from all functions, loading all required symbols, the AppImage path etc. once
 // to improve performance
 
 // DRY
 static const char proc_self_exe[] = "/proc/self/exe";
 
-// get rid of $LD_PRELOAD in the first binary which this library is preloaded into (should be the runtime)
-// the easiest way is to wait for one of these functions to be used, then unset it
-void __clear_env_var() {
-    unsetenv("LD_PRELOAD");
+
+void __init() {
+    static bool initialized = false;
+
+    if (!initialized) {
+        initialized = true;
+
+        // get rid of $LD_PRELOAD in the first binary which this library is preloaded into (should be the runtime)
+        // the easiest way is to wait for one of these functions to be used, then unset it
+        unsetenv("LD_PRELOAD");
+
+        // load symbols from libc
+        __libc_readlink = (ssize_t (*) (const char*, void*, size_t)) dlsym(REAL_LIBC, "readlink");
+        __libc_realpath = (char* (*) (const char*, char*)) dlsym(REAL_LIBC, "realpath");
+        __libc_open = (int (*) (const char*, int)) dlsym(REAL_LIBC, "open");
+
+        if (__libc_readlink == NULL || __libc_realpath == NULL || __libc_open == NULL) {
+            log_error("failed to load symbol from libc\n");
+            exit(EXIT_CODE_FAILURE);
+        }
+    }
 }
 
 char* __abs_appimage_path() {
-    // TODO: create a macro for this pattern (DRY)
-    static char* (*libc_realpath)(const char*, char*) = NULL;
-    if (libc_realpath == NULL) {
-        libc_realpath = (char* (*) (const char*, char*)) dlsym(REAL_LIBC, "realpath");
-    }
+    __init();
 
     static const char env_var_name[] = "REDIRECT_APPIMAGE";
 
@@ -67,13 +88,7 @@ char* __abs_appimage_path() {
 }
 
 ssize_t readlink(const char* path, char* buf, size_t len) {
-    __clear_env_var();
-    
-    // if necessary, we load the real read(...) symbol from libc
-    static ssize_t (*libc_readlink)(const char*, void*, size_t) = NULL;
-    if (libc_readlink == NULL) {
-        libc_readlink = (ssize_t (*) (const char*, void*, size_t)) dlsym(REAL_LIBC, "readlink");
-    }
+    __init();
 
     log_debug("readlink %s, len %ld\n", path, len);
 
@@ -92,16 +107,11 @@ ssize_t readlink(const char* path, char* buf, size_t len) {
         return ret;
     }
 
-    return libc_readlink(path, buf, len);
+    return __libc_readlink(path, buf, len);
 }
 
 char* realpath(const char* name, char* resolved) {
-    __clear_env_var();
-
-    static char* (*libc_realpath)(const char*, char*) = NULL;
-    if (libc_realpath == NULL) {
-        libc_realpath = (char* (*) (const char*, char*)) dlsym(REAL_LIBC, "realpath");
-    }
+    __init();
 
     log_debug("realpath %s, %s\n", name, resolved);
 
@@ -120,7 +130,7 @@ char* realpath(const char* name, char* resolved) {
         return resolved;
     }
 
-    char* retval = libc_realpath(name, resolved);
+    char* retval = __libc_realpath(name, resolved);
 
     log_debug("realpath result: %s -> %s, retval %s\n", name, resolved, retval);
 
@@ -129,13 +139,7 @@ char* realpath(const char* name, char* resolved) {
 
 // used by squashfuse, specifically util.c/sqfs_fd_open
 int open(const char* file, int flags, ...) {
-    __clear_env_var();
-
-    // if necessary, we load the real read(...) symbol from libc
-    static int (*libc_open)(const char*, int) = NULL;
-    if (libc_open == NULL) {
-        libc_open = (int (*) (const char*, int)) dlsym(REAL_LIBC, "open");
-    }
+    __init();
 
     log_debug("open(%s, %d)\n", file, flags);
 
@@ -147,7 +151,7 @@ int open(const char* file, int flags, ...) {
         file = abspath;
     }
 
-    int result = libc_open(file, flags);
+    int result = __libc_open(file, flags);
 
     if (abspath != NULL) {
         free(abspath);
